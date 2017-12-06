21e0b9261804404e22d5fc83721b9ef708a4c6cb
derek-haworth <derekhaw1@gmail.com>
2017-12-05 19:56:40 -0600

    set up load function and add underscore to words

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,4 +1,5 @@
-var words = ["Angry Beavers", "Animaniacs", "Dexters Lab", "Doug", "DuckTales", "PowerPuff Girls", "Rugrats", "TaleSpin", "Simpsons" ];
+var wordsArr = ["Angry Beavers", "Animaniacs", "Dexters Lab", "Doug", "DuckTales", "PowerPuff Girls", "Rugrats", "TaleSpin", "Simpsons" ];
+
 
 var wins = 0;
 var losses = 0;
@@ -6,15 +7,40 @@ var guesses = 0;
 
 
 
+// Execute Game after refresh/inital load
+window.onload = newGame();
+
+function newGame() {
+
+	var word = wordsArr[Math.floor(Math.random() * wordsArr.length)];
+	// Use RegEx to add underscores
+	var underscores = word.replace(/.{1}/g, "_ ");
+	document.getElementById("placeholder").innerHTML = underscores;
+	
+	//Add underscores
+	var underscores = "";
+	for ( i = 0; i < word.length; i++) {
+		//if (word contains spaces) {
+			//split the word
+		//} else {
+			underscores = underscores + "_ "
+		//}
+	}
+}
+
+
+
 document.onkeyup = function(event) {
-	var userGuess = event.key;
-	var choices = /[a-zA-Z]+/g;
+	var userGuess = event.key.toLowerCase();
+	var choices = /[a-z]+/g;
 	var res = userGuess.match(choices);
 
-	document.getElementById("demo").innerHTML = res;
+	if (!res) {
+		alert("Letters Only, Please");  
+	}
 
-	 if (!res){
-	    alert("Letters Only, Please");  
-	 }
+	// var word = wordsArr[Math.floor(Math.random() * wordsArr.length)];
+	// var underscores = word.replace(/.{1}/g, "_ ");
 
+	// document.getElementById("placeholder").innerHTML = underscores;
 }
