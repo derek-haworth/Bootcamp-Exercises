c62cc0dbec213420d40b1db0e03731de2055a8bd
derek-haworth <derekhaw1@gmail.com>
2017-12-07 14:38:13 -0600

    add working logic file hang.js

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,104 +1,64 @@
-var game = {
-  words: {
-    angryBeavers: {
-      title: "AngryBeavers",
-      image: "assets/images/angryBeavers.png",
-    },
-    animaniacs: {
-      title: "Animaniacs",
-      image: "assets/images/animaniacs.jpg",
-    },
-    dextersLab: {
-      title: "DextersLab",
-      image: "assets/images/dextersLab.jpg",
-    },
-    doug: {
-      title: "Doug",
-      image: "assets/images/doug.jpg",
-    },
-    duckTales: {
-      title: "DuckTales",
-      image: "assets/images/duckTales.jpg",
-    },
-    powerPuff: {
-      title: "PowerPuffGirls",
-      image: "assets/images/powerPuff.jpg",
-    },
-    rugrats: {
-      title: "Rugrats",
-      image: "assets/images/rugrats.jpg",
-    },
-    talespin: {
-      title: "Talespin",
-      image: "assets/images/taleSpin.jpg",
-    },
-    simpsons: {
-      title: "Simpsons",
-      image: "assets/images/Simpsons.jpg",
-    }
-  },
-  	word: null,
-  	letters: [],
-	  matchedLetters: [],
-  	guessedLetters: [],
-  	totalGuesses: 0,
-  	wins: 0,
-	  losses: 0,
-    placeholder: "",
+var wordArr = ["angrybeavers", "animaniacs", "dexterslab", "doug", "ducktales", 
+                "powerpuffgirls", "rugrats","simpsons", "talespin"];
 
-	getGame: function() {
-		var objKeys = Object.keys(this.words);
-		this.word = objKeys[Math.floor(Math.random() * objKeys.length)].toLowerCase();
-		this.letters = this.word.split("");
-    
-	  //Add underscores
-    for (var i = 0; i < this.letters.length; i++) {
-        this.placeholder += "_ ";
-    }
-    document.getElementById("currentWord").innerHTML = this.placeholder;
+var randWord = wordArr[Math.floor(Math.random() * wordArr.length)].toLowerCase();
+var wins = 0;
+var losses = 0;
+var wordString;
+var guessesLeft = 10;
+var guessArr = [];
+
+function initialize() {
+  guessesLeft = 10;
+  for (var i = 0; i < randWord.length; i++) {
+    guessArr[i] = "__ ";
   }
 
-};
-
-var lettersGuessed = "";
-var guessesLeft = 10;
-
-window.onload = game.getGame();
-document.onkeyup = function(event) {
-	lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-	var correct = 0;
-
-	document.getElementById("guessLetters").innerHTML += lettersGuessed;
-	document.getElementById("guessRemain").innerHTML = guessesLeft;
-
-	for (var i = 0; i < game.letters.length; i++) {
-		if (lettersGuessed == game.word.substring(i, i + 1)) {
-			correct++;
-			game.placeholder = game.placeholder.substring(0, i) + lettersGuessed + game.placeholder.substring(i +1, game.placeholder.length +1);
-			document.getElementById("currentWord").innerHTML = game.placeholder;
-		}
-	}
-	
-	if (correct === 0) {
-	  	guessesLeft--;
-	}
-
-	if (game.placeholder.indexOf("_") == -1) {
-		game.wins++;
-		var userWins = game.wins;
-		document.querySelector("#wins").innerHTML = userWins;
-		document.getElementById("image").src = game.words[game.word].image;
-
-		var correctGuess = " ";
-		document.querySelector("#guessLetters").innerHTML = correctGuess;
-		game.getGame();
-	}
-	
-	if (guessesLeft === -1) {
-		game.losses++;
-		var userLoses = game.losses;
-		document.querySelector("#losses").innerHTML = userLoses;
-		game.getGame();
-	}
+  wordString = guessArr.join("");
+  document.getElementById("currentWord").innerHTML = wordString;
 }
+
+
+document.onkeyup = function(event) {
+  var res = "";
+  var userGuess = event.key;
+  var choices = /[a-zA-Z0-9]+/g;
+  res = userGuess.match(choices);
+  var guesses = 0;
+  document.getElementById("guessLetters").innerHTML += res;    
+
+  if (res) {
+    for (var i = 0; i < randWord.length; i++) {
+      if (randWord[i] === res) {
+        alert("working");
+        guessArr[i].appendChild(res);
+      }
+    }
+    guesses++;
+    document.getElementById("guessRemain").innerHTML = guessesLeft--;
+    document.getElementById("currentWord").innerHTML = guessArr.join(" ");
+
+    if (guesses === 0) {
+      guessesLeft--;
+    }
+    if (guessesLeft === -1) {
+      losses++;
+      document.querySelector("#losses").innerHTML = losses;
+      initialize();
+    } else if ( guessArr[i].indexOf("__") == -1) {
+      wins++;
+      document.querySelector("#wins").innerHTML = wins;
+      initialize();
+    }
+  //non number or letter entered
+  } else {
+    alert("Please use alpha-numerical values");
+  }
+
+} // end onkeyup
+
+
+//start game
+initialize();
+
 
---
+++ Hangman-Game/assets/javascript/hang.js
@@ -0,0 +1,106 @@
+var wins = 0,
+    losses = 0,
+    guessesRemain = "?",
+    imageOuput,
+    gameStarted = false,
+
+    //arrays
+    wordArr = ["Angry Beavers", "Animaniacs", "Dexters Lab", "Doug", "Ducktales", "Powerpuff Girls", "Rugrats","Simpsons", "Talespin", "Tiny Toon Adventures", "Pokemon", "Rockos Modern Life", "Hey Arnold", "CatDog", "Ren and Stimpy"],
+    keys = [],
+    currentWord = [],
+    wrongGuesses = [],
+    randomCharacter = [],
+    randomWord = [],
+    chosenWord = [],
+    imageOuput,
+    imageLetters = [];
+
+
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("guessesRemain").innerHTML = guessesRemain;
+
+
+// Creates alphabet array
+for (var i = 65; i <= 90; i++) {
+    keys[keys.length] = String.fromCharCode(i); // will spit out Uppercase
+};
+
+function initialize() {
+
+    gameStarted = true;
+    currentWord = [];
+    wrongGuesses = [];
+    imageLetters = [];
+    
+    randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+    chosenWord = randomWord.toUpperCase();
+    guessesRemain = randomWord.length + 2;
+   
+    for (var i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] === " ") {
+            // Puts a space if word has spance
+            currentWord[i] = " ";
+        } else {
+            // Puts underscores as placeholders
+            currentWord[i] = "_"; 
+            // Creating array to be used for image source
+            imageLetters[i] = randomWord[i].toLowerCase();
+        }
+
+        var placeHolder = currentWord.join("");    
+    };
+
+    document.getElementById("currentWord").innerHTML = placeHolder;
+    //image source
+    imageOuput = imageLetters.join("");
+};
+
+initialize();
+    
+document.onkeyup = function(event) {
+    var userGuess = event.key.toUpperCase();
+    var isGuessCorrect = false;
+
+    // Is keys part of the alphabet?
+    if(keys.includes(userGuess)) {        
+        // Checks for a match
+        for (var j = 0; j < chosenWord.length; j++) {    
+            if (chosenWord[j] === userGuess) {
+                currentWord[j] = userGuess;
+                isGuessCorrect = true;
+            }
+        }
+    }
+
+    // Subtract from guess && add letter to guessed letters
+    if(!wrongGuesses.includes(userGuess) && keys.includes(userGuess) && !chosenWord.includes(userGuess)) {
+        guessesRemain--;
+        wrongGuesses.push(userGuess);
+    }
+
+    // Concatenate correctly guessed letters
+    var winningWord = currentWord.join("");
+    
+    //wins
+    if (winningWord === chosenWord) {
+        document.getElementById("image").setAttribute("src", "assets/images/" + imageOuput +".jpg");
+        wins++;
+        initialize();
+    }
+    //losses
+    if (guessesRemain === 0) {
+        alert(randomCharacter);
+        document.getElementById("image").setAttribute("src", "assets/images/" + imageOuput +".jpg");
+        losses++;
+        initialize();
+    }
+
+    document.getElementById("guessLetters").innerHTML = wrongGuesses.join(" ");
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("guessesRemain").innerHTML = guessesRemain;
+    document.getElementById("currentWord").innerHTML = currentWord.join("");
+    
+
+};
