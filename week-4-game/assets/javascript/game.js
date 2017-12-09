58964531b81fce0c39223dc0a3f0b48ad7678cad
derek-haworth <derekhaw1@gmail.com>
2017-12-09 10:29:38 -0600

    set up html and js objects for game

---
+++ week-4-game/assets/javascript/game.js
@@ -0,0 +1,22 @@
+var Player = function(name, hp, attackPower, attackPowerConst, counter) {
+  this.name = name;
+  this.hp = hp;
+  this.attackPower = attackPower;
+  this.attackPowerConst = attackPowerConst;
+  this.counter = counter;
+}
+
+var playerOne = new Player("Derek", 100, 20, 20, 25);
+var playerTwo = new Player("Derek", 100, 20, 20, 25);
+var playerThree = new Player("Derek", 100, 20, 20, 25);
+var playerFour = new Player("Derek", 100, 20, 20, 25);
+
+//Look into jQuery method for Constructors
+
+$(document).ready(function() {
+	
+
+
+
+	
+});
