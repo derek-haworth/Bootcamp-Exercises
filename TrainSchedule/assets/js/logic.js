e90eb536a66aa2d82e40e14d04f77dbfcadc78cb
Derek Haworth <derek_haworth@bradycorp.com>
2018-01-16 12:00:44 -0600

    add momentjs functionality

--- TrainSchedule/assets/js/logic.js
+++ TrainSchedule/assets/js/logic.js
@@ -1,3 +1,13 @@
+var nextTrain = '',
+	nextTrainFormatted = '',
+	minutesAway = '',
+	firstTimeConverted = '',
+	currentTime = '',
+	diffTime = '',
+	timeRemain = '',
+	minutesTilTrain = '';
+
+
 $(document).ready(function(){
 
 // Initialize Firebase
@@ -14,44 +24,74 @@ $(document).ready(function(){
 
 	var database = firebase.database();
 
-	$("#addEmployee").on("click", function() {
+	$("#addTrain").on("click", function() {
 
 		event.preventDefault();
 
 		var name = $("#name").val().trim();
-		var role = $("#role").val().trim();
+		var destination = $("#destination").val().trim();
 		var frequency = $("#frequency").val().trim();
-		var time = $("#time").val();
+		var firstTrain = $("#time").val().trim();
 
-		var newEmployee = {
+		firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+		currentTime = moment();
+		diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		timeRemain = diffTime % frequency;
+		minutesTilTrain = frequency = timeRemain;
+		nextTrain = moment().add(minutesTilTrain, "minutes");
+		nextTrainFormatted = moment(nextTrain).format("hh:mm");
+
+		var newTrain = {
 			name: name,
-			role: role,
-			start: time,
-			frequency: frequency
+			destination: destination,
+			//start: firstTrain,
+			frequency: frequency,
+			//nextTrainFormatted: nextTrainFormatted,
+			minutesTilTrain: minutesTilTrain
 		}
 
 		// Upload to the database
-		database.ref().push(newEmployee);
+		database.ref().push(newTrain);
 
 		//clear out the fields after submitting
 		name = $("#name").val("");
-		role = $("#role").val("");
+		destination = $("#destination").val("");
 		frequency = $("#frequency").val("");
-		start = $("#time").val("");
+		firstTrain = $("#time").val("");
 
 	});// end on-click event
 
 	database.ref().on("child_added", function(childSnapshot) {
 
+		var rowId = database.ref().key;
+		console.log(rowId);
+
 		// Store it
 		var name = childSnapshot.val().name;
-		var role = childSnapshot.val().role;
+		var destination = childSnapshot.val().destination;
 		var start = childSnapshot.val().start;
 		var frequency = childSnapshot.val().frequency;
+		var nextTrain = childSnapshot.val().nextTrainFormatted;
+		var minAway = childSnapshot.val().minutesTilTrain;
 
-		$("#trainRows").prepend("<tr><td>" + name + "</td><td>" + role + "</td><td>" + frequency + "</td><td>" + start + "</td><td>" + "x" + "</td></tr>");
+		$("#trainRows").prepend(
+			"<tr id=" + "'" + rowId + "'" + "><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + minAway + "</td><td>" + "<input type='submit' value='Remove' class='remove-train btn btn-primary btn-sm'></td></tr>"
+		);
 
 	});
+
+	$("body").on("click", ".remove-train", function() {
+		debugger;
+
+		// DEBUGGING
+
+		// When clicking delete, find the row associated with the button - delete it
+		// Next find the id of said row and remove that key value from the database
+    	$(this).closest("tr").remove();
+    	var getKey = $(this).parent().parent().attr("id");
+    	database.ref().child(getKey).remove();
+	});
+
 
 
 });
